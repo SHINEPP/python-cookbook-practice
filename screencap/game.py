08157d0f52d9e159cbf46d0dde19aab263d3944e
zhouzhenliang <zhouzhenliangpp@163.com>
2024-03-01 11:43:50 +0800

    调用adb截图并保存在电脑本地

---
+++ screencap/game.py
@@ -0,0 +1,12 @@
+import os
+from datetime import datetime
+
+if __name__ == '__main__':
+    now = datetime.now()
+    ftime = now.strftime('%Y%m%d%H%M%S-%f')
+    phone_dir = '/sdcard/android_adb_zzl'
+    local_dir = '/Users/zhouzhenliang/temp'
+    os.system(f'adb shell mkdir {phone_dir}')
+    os.system(f'adb shell screencap -p {phone_dir}/{ftime}.png')
+    os.system(f'adb pull {phone_dir}/{ftime}.png {local_dir}')
+    os.system(f'adb shell rm -f {phone_dir}/{ftime}.png')
